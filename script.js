437264bb765713812edea0832e50d828473c76e4
unknown <elene.kikalishvili.1@btu.edu.ge>
2020-03-04 14:39:03 +0400

    added check functionality whether input value contains whitespaces or not

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var button = document.getElementById("enter");
+let input = document.getElementById("userinput");
+let ul = document.querySelector("ul");
+let button = document.getElementById("enter");
 
 function createListElement() {
-  var newDiv = document.createElement("div");
+  let newDiv = document.createElement("div");
   ul.appendChild(newDiv);
-  var newLi = document.createElement("li");
+  let newLi = document.createElement("li");
   newLi.appendChild(document.createTextNode(input.value));
   //append list element in newDiv
   newDiv.appendChild(newLi);
   //create coresponding delbutton
-  var delBtn = document.createElement("button");
+  let delBtn = document.createElement("button");
   delBtn.appendChild(document.createTextNode("delete"));
   delBtn.addEventListener("click", () => newDiv.removeChild(newLi));
   newLi.appendChild(delBtn);
