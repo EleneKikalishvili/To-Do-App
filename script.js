ff4c33375622bac208a5e8ff631524a372545c7e
unknown <elene.kikalishvili.1@btu.edu.ge>
2020-03-06 13:41:19 +0400

    created form element, input field, submit button and replaced event listeners with onsubmit event listener with same functionality

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-let input = document.getElementById("userinput");
-let ul = document.querySelector("ul");
-let button = document.getElementById("enter");
+let body = document.querySelector("body");
+
+let createform = document.createElement("form");
+body.appendChild(createform);
+
+let container = document.createElement("div");
+createform.appendChild(container);
+
+//creating input field
+let input = document.createElement("input");
+input.setAttribute("type", "text");
+input.setAttribute("placeholder", "enter items");
+container.appendChild(input);
+
+//creating submit button
+let submitElement = document.createElement("input");
+submitElement.setAttribute("type", "submit");
+container.appendChild(submitElement);
+
+//creating ul element
+let ul = document.createElement("ul");
+container.appendChild(ul);
 
 function createListElement() {
   let newDiv = document.createElement("div");
   ul.appendChild(newDiv);
   let newLi = document.createElement("li");
   newLi.appendChild(document.createTextNode(input.value));
-  //append list element in newDiv
   newDiv.appendChild(newLi);
+
   //create rename button
   let renBtn = document.createElement("button");
   renBtn.appendChild(document.createTextNode("rename"));
@@ -17,6 +36,7 @@ function createListElement() {
     "click",
     () => (newLi.innerHTML = window.prompt("make changes: "))
   );
+
   //create coresponding delbutton
   let delBtn = document.createElement("button");
   delBtn.appendChild(document.createTextNode("delete"));
@@ -26,18 +46,8 @@ function createListElement() {
   input.value = "";
 }
 
-//add List after click
-button.addEventListener("click", () => {
+createform.onsubmit = e => {
   if (input.value.length > 0 && input.value.replace(/\s/g, ""))
     createListElement();
-});
-
-//add list after keypress
-input.addEventListener("keypress", event => {
-  if (
-    input.value.length > 0 &&
-    event.keyCode === 13 &&
-    input.value.replace(/\s/g, "")
-  )
-    createListElement();
-});
+  e.preventDefault();
+};
